6f512223348de991ada98db34a9df24f6a6e77b6
Tadeo <tadeolozanog2@gmail.com>
2022-05-31 18:48:23 -0500

    Subida de los objetos e imagenes del menu

--- MyWorld.java
+++ MyWorld.java
@@ -16,6 +16,22 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1000, 900, 1); 
+        prepare(); 
+    }
+    
+    private void prepare()
+    {
+        Start start = new Start();
+        addObject(start,500,500);
+        start.setLocation(400,830);
+        Exit exit = new Exit();
+        addObject(exit,222,194);
+        Info info = new Info();
+        addObject(info,200,830);
+        Score score = new Score();
+        addObject(score,353,269);
+        exit.setLocation(600,830);
+        score.setLocation(800,830);
     }
 }
---
+++ Start.java
@@ -0,0 +1,8 @@
+import greenfoot.*; 
+
+public class Start extends Button
+{
+    public  void clicked(){
+        Greenfoot.setWorld(new NivelUno());
+    }
+}
